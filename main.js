8833f027a76e75890afc28250955d6e676cba23e
rantiindriyani <121720015+rantiindriyani@users.noreply.github.com>
2023-09-25 00:40:13 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// Deklarasi variabel-variabel identitas
+const nama = "RANTI INDRIYANI";
+const nim = "20220040002";
+
+// Deklarasi variabel-variabel untuk operasi aritmatika
+const angka1 = 10;
+const angka2 = 5;
+
+// Menampilkan identitas
+console.log("Identitas:");
+console.log("Nama: " + nama);
+console.log("NiM: " + nim);
+// Melakukan operasi aritmatika sederhana
+const tambah = angka1 + angka2;
+const kurang = angka1 - angka2;
+const kali = angka1 * angka2;
+const bagi = angka1 / angka2;
+
+// Menampilkan hasil operasi aritmatika
+console.log("Hasil Penambahan: " + tambah);
+console.log("Hasil Pengurangan: " + kurang);
+console.log("Hasil Perkalian: " + kali);
+console.log("Hasil Pembagian: " + bagi);
